53f9c7bb5ad252564b89803c16cbb847071c6f34
luka <lukaogadze@live.com>
2018-05-18 18:53:13 +0400

    added comments to JSEvents Exercises

--- JSTutorial/JSEvents/Exercises/Exercise2/js/app.js
+++ JSTutorial/JSEvents/Exercises/Exercise2/js/app.js
@@ -1,5 +1,8 @@
 "use strict";
 // When the button is clicked, trigger myFunction() with an event.
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+// }
 document.body.onload = function () {
     var btn = document.getElementsByTagName("button")[0];
     function myFunction() {
--- JSTutorial/JSEvents/Exercises/Exercise2/js/app.ts
+++ JSTutorial/JSEvents/Exercises/Exercise2/js/app.ts
@@ -1,4 +1,7 @@
 // When the button is clicked, trigger myFunction() with an event.
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+// }
 document.body.onload = function (): void {
     const btn: HTMLButtonElement = document.getElementsByTagName("button")[0];
     function myFunction(): void {
