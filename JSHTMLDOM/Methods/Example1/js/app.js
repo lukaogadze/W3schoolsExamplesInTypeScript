12678ed898b37cb4edb129f9e10cdc42b747fa43
luka <lukaogadze@live.com>
2018-06-28 10:00:38 +0400

    added Methods

---
+++ JSHTMLDOM/Methods/Example1/js/app.js
@@ -0,0 +1,5 @@
+"use strict";
+window.onload = function () {
+    document.getElementById("demo").innerHTML = "Hello World!";
+};
+//# sourceMappingURL=app.js.map
---
+++ JSHTMLDOM/Methods/Example1/js/app.ts
@@ -0,0 +1,3 @@
+window.onload = function (): void {
+    document.getElementById("demo")!.innerHTML = "Hello World!";
+};
