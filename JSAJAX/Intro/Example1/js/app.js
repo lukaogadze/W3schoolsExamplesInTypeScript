9c4a46e5b66a9cf251945bedf73edf7ff73d4cde
luka ogadze <lukaogadze@live.com>
2018-07-06 17:57:53 +0400

    Intro update

--- JSAJAX/Intro/Example1/js/app.js
+++ JSAJAX/Intro/Example1/js/app.js
@@ -3,14 +3,11 @@ window.onload = function () {
     // Cross-Origin Request Blocked... fk!!!
     function loadDoc() {
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("demo").innerHTML =
-                    this.responseText;
-            }
-        };
-        xhttp.open("GET", "https://www.w3schools.com/js/ajax_info.txt", true);
+        xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
         xhttp.send();
+        xhttp.onreadystatechange = function () {
+            document.getElementById("demo").innerHTML = this.responseText;
+        };
     }
     document.getElementsByTagName("button")[0].onclick = loadDoc;
 };
--- JSAJAX/Intro/Example1/js/app.ts
+++ JSAJAX/Intro/Example1/js/app.ts
@@ -1,15 +1,12 @@
 window.onload = () => {
     // Cross-Origin Request Blocked... fk!!!
     function loadDoc(): void {
-        const xhttp: XMLHttpRequest = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("demo")!.innerHTML =
-                    this.responseText;
-            }
-        };
-        xhttp.open("GET", "https://www.w3schools.com/js/ajax_info.txt", true);
+        const xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
         xhttp.send();
+        xhttp.onreadystatechange = function () {
+            document.getElementById("demo")!.innerHTML = this.responseText;
+        };
     }
 
     document.getElementsByTagName("button")[0].onclick = loadDoc;
